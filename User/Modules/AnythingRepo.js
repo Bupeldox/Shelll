8d10a53fff5173e1920643bbc4a2b1c89b4d862b
John Warren <32330519+Bupeldox@users.noreply.github.com>
2025-12-02 15:06:56 +0000

    Create AnythingRepo.js

---
+++ User/Modules/AnythingRepo.js
@@ -0,0 +1,38 @@
+
+class AnythingRepo{
+    constructor(){
+        this.items = {};
+    }
+    save(item){
+        var id = item.$anythingRepoId;
+        if(!id){
+            id = Math.random();
+            item.$anythingRepoId = id;
+        }
+        this.items[id] = item;
+    }
+    remove(item){
+        //Can only remove something that was added to this repo.
+        var id = item.$anythingRepoId;
+        delete this.items[id];
+        delete item.$anythingRepoId;
+    }
+    getAll(){
+        return Object.values(this.items);
+    }
+}
+
+
+export default{
+    Dependencies:[],
+    module:AnythingRepo,
+    Interface:{
+        name:"IRepo",
+        functions:[
+            {name:"add",param:["any"]},
+            {name:"getAll",param:false},
+            {name:"remove",param:["any"]},
+        ]
+    },
+    name:"AnythingRepo"
+}
